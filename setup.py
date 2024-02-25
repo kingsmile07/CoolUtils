fd4ab37e21e7084d44cd6514041fcc5978f432c7
kingsmile <wusicheng@gmail.com>
2024-02-25 11:53:31 +0800

    第一次更新

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     name='CoolUtils',
     version='1.0.0',
     description='A collection of cool and useful utilities.',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='kingsmile',
+    author_email='wusicheng@gmail.com',
     packages=find_packages(),
     install_requires=[
         'requests>=2.25.0',
